850c7701aa11755bdbd9d5b17c9b6a660e48e6d3
freek022 <fkg333@gmail.com>
2015-03-19 16:26:50 +0000

    added navigation bar customised with bootstrap

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def courses
+  end
+
+  def locations
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get courses" do
+    get :courses
+    assert_response :success
+  end
+
+  test "should get locations" do
+    get :locations
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+end
