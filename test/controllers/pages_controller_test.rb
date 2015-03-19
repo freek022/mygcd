a1537fc146e3821b28268c7f7408b1acca6b2e27
freek022 <fkg333@gmail.com>
2015-03-19 22:24:46 +0000

    added title to pages layout

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,26 +4,31 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Griffith College"
   end
 
   test "should get courses" do
     get :courses
     assert_response :success
+    assert_select "title", "Courses | Find Courses"
   end
 
   test "should get locations" do
     get :locations
     assert_response :success
+    assert_select "title", "Locations | Campus Locations"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | About Us"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Contact Us"
   end
 
 end
